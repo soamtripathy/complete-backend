287cc77e648dc3e27a541d3ae640cf0b55122a2d
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-09-02 21:15:48 +0530

    add auth.middleware, login and logout features

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
@@ -16,5 +21,10 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post( loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
